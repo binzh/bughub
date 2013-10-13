233ef3e17fa9af2415310993728e761f3f2b2d4f
binzh <binzh84@gmail.com>
2013-10-13 20:25:32 +0800

    修正devise的错误提示以及token错误

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,10 +1,14 @@
 module DeviseHelper
   def devise_error_messages!
-    return "" if resource.errors.empty?
+    return '[]' if resource.errors.empty?
     resource.errors.full_messages
   end
 
   def devise_error_messages?
     resource.errors.empty? ? false : true
+  end
+
+  def resource
+    @resource ||= User.new
   end
 end
